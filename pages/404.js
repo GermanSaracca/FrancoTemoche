3dd04907fe94d5c0b627b69b0f6df74f184dc63f
gsaracca <gersaracca@gmail.com>
2021-11-04 21:56:39 -0300

    404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFoundPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 4000)
+  }, [])
+
   return (
     <div>
       <h1>No se encontro la pagina</h1>
+      <p>
+        Volver a la <Link href="/">Homepage</Link>
+      </p>
       <style jsx>{`
         div {
           background: pink;
